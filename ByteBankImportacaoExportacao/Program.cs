1ff4d1730448cce23acfedf69c29edee721d3152
Guilherme Matheus Costa <costa.guimatheus@gmail.com>
2018-08-10 19:55:55 +0000

    Projeto da aula 1 - Lendo arquivos de texto

---
+++ ByteBankImportacaoExportacao/Program.cs
@@ -0,0 +1,49 @@
+﻿using ByteBankImportacaoExportacao.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO; // IO = Input e Output
+
+
+namespace ByteBankImportacaoExportacao 
+{ 
+    class Program 
+    { 
+        static void Main(string[] args) 
+        {
+            var enderecoDoArquivo = "contas.txt";
+
+            var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open);
+
+            var buffer = new byte[1024]; // 1 kb
+            var numeroDeBytesLidos = -1;
+
+            while(numeroDeBytesLidos != 0)
+            {
+                numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
+                EscreverBuffer(buffer);
+            }
+
+            Console.ReadLine();
+        }
+
+        static void EscreverBuffer(byte[] buffer)
+        {
+            var utf8 = Encoding.Default;
+
+            var texto = utf8.GetString(buffer);
+            Console.Write(texto);
+
+            //foreach (var meuByte in buffer)
+            //{
+            //    Console.Write(meuByte);
+            //    Console.Write(" ");
+            //}
+        }
+
+    }
+} 
+ 
