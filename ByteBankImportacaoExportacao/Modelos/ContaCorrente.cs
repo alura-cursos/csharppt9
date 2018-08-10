0400094623c08312cfb23ed29c8bc84c834e3515
Guilherme Matheus Costa <costa.guimatheus@gmail.com>
2018-08-10 19:56:01 +0000

    Projeto da aula 2 - Mais FileStream e leitor StreamReader

---
+++ ByteBankImportacaoExportacao/Modelos/ContaCorrente.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBankImportacaoExportacao.Modelos
+{
+    public class ContaCorrente
+    {
+        public int Numero { get; }
+        public int Agencia { get; }
+        public double Saldo { get; private set; }
+        public Cliente Titular { get; set; }
+
+        public ContaCorrente(int agencia, int numero)
+        {
+            Agencia = agencia;
+            Numero = numero;
+        }
+
+        public void Depositar(double valor)
+        {
+            if (valor <= 0)
+            {
+                throw new ArgumentException("Valor de deposito deve ser maior que zero.", nameof(valor));
+            }
+
+            Saldo += valor;
+        }
+
+        public void Sacar(double valor)
+        {
+            if (valor <= 0)
+            {
+                throw new ArgumentException("Valor de saque deve ser maior que zero.", nameof(valor));
+            }
+
+            if(valor > Saldo)
+            {
+                throw new InvalidOperationException("Saldo insuficiente.");
+            }
+
+            Saldo += valor;
+        }
+    }
+}
---
+++ ByteBankImportacaoExportacao/Program.cs
@@ -0,0 +1,38 @@
+﻿using ByteBankImportacaoExportacao.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO; // IO = Input e Output
+
+
+namespace ByteBankImportacaoExportacao 
+{ 
+    partial class Program 
+    { 
+        static void Main(string[] args) 
+        {
+            var enderecoDoArquivo = "contas.txt";
+
+            using (var fluxoDeArquivo = new FileStream(enderecoDoArquivo, FileMode.Open))
+            using (var leitor = new StreamReader(fluxoDeArquivo))
+            {
+                while (!leitor.EndOfStream)
+                {
+                    var linha = leitor.ReadLine();
+                    Console.WriteLine(linha);
+                } 
+            }
+            
+
+
+            Console.ReadLine();
+        }
+
+        
+
+    }
+} 
+ 
