54b29211402b2632ad0da7354a5e889b0873437a
Guilherme Matheus Costa <costa.guimatheus@gmail.com>
2018-08-10 19:56:14 +0000

    Projeto da aula 4 - O Flush e arquivos binarios

---
+++ ByteBankImportacaoExportacao/Program.cs
@@ -0,0 +1,27 @@
+﻿using ByteBankImportacaoExportacao.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO; // IO = Input e Output
+using System.Globalization;
+
+namespace ByteBankImportacaoExportacao 
+{ 
+    partial class Program 
+    { 
+        static void Main(string[] args) 
+        {
+            LeituraBinaria();
+
+
+            Console.WriteLine("Aplicação finalizada. . .");
+
+
+            Console.ReadLine();
+        }
+    }
+} 
+ 
